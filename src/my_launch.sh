faad6ba23d5dd5895427df0d5c22374841326925
TWBauer <kevinsantiagomontana@gmail.com>
2022-08-18 10:43:23 -0500

    Repositorio con Readme completo

--- src/my_launch.sh
+++ src/my_launch.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
 clear
+
+
+
 echo "Lanzando ros"
 roscore &
 sleep 5
 echo "Ros ok"
+$PWD
 
 echo "Lanzando Talker"
-python3 /home/$USER/Desktop/Inteligencia\ Artificial/talker.py &
+python3 $PWD/talker.py &
 echo "Talker ok"
 
 echo "Lanzando Listener"
-python3 /home/$USER/Desktop/Inteligencia\ Artificial/listener.py &
+python3 $PWD/listener.py &
 echo "Listener ok"
